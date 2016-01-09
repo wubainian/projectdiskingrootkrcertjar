c2b6eabd83317edb3e78cdd29a93d1a8b51eee30
wubainian <jinangl@163.com>
2016-01-09 11:46:52 +0800

    init

---
+++ gen/wubainian/vsnake/projectdiskingrootkrcertjar/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package wubainian.vsnake.projectdiskingrootkrcertjar;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
